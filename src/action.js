5324bfb66987b9d875f02957f57180dcdc302984
vibi-230 <vibin@turftown.in>
2021-04-25 22:41:24 +0530

    base url updated

--- src/action.js
+++ src/action.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://google-map-img-be.herokuapp.com/",
   // timeout: 1000,
 });
 
